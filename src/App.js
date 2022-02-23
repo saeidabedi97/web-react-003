672651123d89521551d4b81017eec5c9c5820621
Saeid <48330269+saeidabedi97@users.noreply.github.com>
2022-02-23 21:07:46 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,20 @@
 import React, { useState } from "react";
 import useSWR from "swr";
 
-const LIST_ALL_BREEDS_ENDPOINT =
-  "https://emojihub.herokuapp.com/api/random/group_face_positive";
+const LIST_ALL_BREEDS_ENDPOINT = "https://emojihub.herokuapp.com/api/all";
 
 const fetchJSON = async (endpoint) =>
   await fetch(endpoint).then((x) => x.json());
 
 const App = () => {
   const { data } = useSWR(LIST_ALL_BREEDS_ENDPOINT, fetchJSON);
+  // const [selectedBreed, setSelectedBreed] = useState();
+  const listOfBreeds = Object(data);
 
-  return <pre>{JSON.stringify(data)}</pre>;
-  // const [selectedBreed, setSelectedBreed] = useState(null);
-  // const listOfBreeds = Object.keys(data.message);
-  // return (
-  //   <div>
-  //     {selectedBreed}
-  //     <ul>
-  //       {listOfBreeds.map((breed) => (
-  //         <li>
-  //           <button onClick={() => setSelectedBreed(breed)}>{breed}</button>
-  //         </li>
-  //       ))}
-  //     </ul>
-  //   </div>
-  //);
+  return (
+    <div>
+      <pre>{JSON.stringify(listOfBreeds, null, 2)};</pre>
+    </div>
+  );
 };
-
 export default App;
